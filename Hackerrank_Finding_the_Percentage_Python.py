e07196aced13e536b5b97e09ec92e4570a1ce7d6
NamanSaini18 <81188700+NamanSaini18@users.noreply.github.com>
2021-09-22 00:26:27 +0530

    Create Hackerrank_Finding_the_Percentage_Python.py

---
+++ Hackerrank_Finding_the_Percentage_Python.py
@@ -0,0 +1,14 @@
+// This is the solution of Hackerrank Python problem "Finding the percentage"
+
+if __name__ == '__main__':
+    n = int(input())
+    marks = {}
+
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        scores=sum(scores)/3
+        marks[name] = scores
+    a = input()    
+    
+    print('%.2f' % marks[a])
